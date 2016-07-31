a76173600f5be368b3de5a363f56a32abecaa93a
Stephen Maitland <s.maitland@me.com>
2016-07-31 01:07:48 +0100

    Final

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var items:[String] = []
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems
+        }
+        table.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+       return items.count
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+    internal func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            table.reloadData()
+            UserDefaults.standard.set(items, forKey: "items")
+        }
+    }
 
 }
 
